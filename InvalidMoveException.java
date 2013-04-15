1c8f81a525d000c2c6eccbdae72038f49af54fbf
jordanknopp <jordan.t.knopp@gmail.com>
2013-04-14 23:33:26 -0700

    Create InvalidMoveException.java
    Included for comprehensiveness - I did not code this.

---
+++ InvalidMoveException.java
@@ -0,0 +1,17 @@
+// InvalidMoveException.java
+//
+// ICS 23 / CSE 23 Fall 2007
+// Project #2: Black and White
+//
+// Minor edits by Norman Jacobson, December 2007
+
+
+// This exception is thrown by the makeMove() method when an attempt is
+// made to make an invalid move.  If your AI throws this exception,
+// it is not working correctly!
+
+
+public class InvalidMoveException
+extends RuntimeException
+{
+}
