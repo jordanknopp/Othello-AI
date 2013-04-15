05cd8884d02478950ec55363067f3ddedb9493ba
jordanknopp <jordan.t.knopp@gmail.com>
2013-04-14 23:35:36 -0700

    Create OthelloCell.java
    Included for comprehensiveness - I did not code this.

---
+++ OthelloCell.java
@@ -0,0 +1,15 @@
+// OthelloCell.java
+//
+// ICS 23 / CSE 23 Fall 2007
+// Project #2: Black and White
+//
+// This enumeration represents the contents of a single cell on an Othello
+// board.  A cell will either have no tile, a white tile, or a black tile.
+
+
+public enum OthelloCell
+{
+  NONE,
+	WHITE,
+	BLACK
+}
